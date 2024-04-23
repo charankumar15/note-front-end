bdcac87f4bc19f147d1356e58b9b9dea20e61309
charankumar15 <158172585+charankumar15@users.noreply.github.com>
2024-04-23 15:39:52 +0530

    Add files via upload

---
+++ python 1/#int float comples.py
@@ -0,0 +1,56 @@
+a =0
+print(type(a))
+
+b= 5.4
+print (type(b))
+
+c=33 + 44j
+print (type(c))
+
+a=10
+b=20
+#add
+#sub
+#mul
+#div
+
+print(a + b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+#bool true,flase
+
+check = True
+print (type(check))
+
+nocheck = False
+print (type (nocheck))
+
+#str
+
+my_str = "charan"
+print (type(my_str))
+
+#concalenation
+
+str1 ="cv"
+str2 ="bv"
+print(str1 + str2)
+
+a ='5'
+print(type(a))
+
+a +10 #int and str add
+
+int(a) + 10
+
+a ='010'
+int(a)
+
+a = '10.5'
+
+int(float(a))
